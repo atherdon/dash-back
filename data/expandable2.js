89e65a498c31b7da0f08e7602d4f4b61fddca416
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-03-16 03:54:03 +0200

    Create expandable2.js

---
+++ data/expandable2.js
@@ -0,0 +1,92 @@
+const expandable2 = [ 
+  {
+    key: 1,
+    name: 'Amy Tom',
+    articles: [
+      'https://app.hackernoon.com/drafts/RTLukSZOLY4dA8i54yu3',
+      'https://app.hackernoon.com/drafts/g8yO5OeEVCP76NWxF9PM',
+      'https://app.hackernoon.com/drafts/Uenv0s9AZjeWwqNS3FHF',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm'
+    ]
+  },
+  {
+    key: 2,
+    name: 'Hackerhodl',
+    articles: [
+      'https://app.hackernoon.com/drafts/RTLukSZOLY4dA8i54yu3',
+      'https://app.hackernoon.com/drafts/g8yO5OeEVCP76NWxF9PM',
+      'https://app.hackernoon.com/drafts/Uenv0s9AZjeWwqNS3FHF',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm'
+    ]
+  },
+  {
+    key: 3,
+    name: 'Natasha',
+    articles: [
+      'https://app.hackernoon.com/drafts/RTLukSZOLY4dA8i54yu3',
+      'https://app.hackernoon.com/drafts/g8yO5OeEVCP76NWxF9PM',
+      'https://app.hackernoon.com/drafts/Uenv0s9AZjeWwqNS3FHF',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm'
+    ]
+  },
+  {
+    key: 4,
+    name: 'Marina',
+    articles: [
+      'https://app.hackernoon.com/drafts/RTLukSZOLY4dA8i54yu3',
+      'https://app.hackernoon.com/drafts/g8yO5OeEVCP76NWxF9PM',
+      'https://app.hackernoon.com/drafts/Uenv0s9AZjeWwqNS3FHF',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm'
+    ]
+  },
+    {
+    key: 5,
+    name: 'David',
+    articles: [
+      'https://app.hackernoon.com/drafts/RTLukSZOLY4dA8i54yu3',
+      'https://app.hackernoon.com/drafts/g8yO5OeEVCP76NWxF9PM',
+      'https://app.hackernoon.com/drafts/Uenv0s9AZjeWwqNS3FHF',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm'
+    ]
+  },
+    {
+    key: 6,
+    name: 'Linh',
+    articles: [
+      'https://app.hackernoon.com/drafts/RTLukSZOLY4dA8i54yu3',
+      'https://app.hackernoon.com/drafts/g8yO5OeEVCP76NWxF9PM',
+      'https://app.hackernoon.com/drafts/Uenv0s9AZjeWwqNS3FHF',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm'
+    ]
+  },
+    {
+    key: 7,
+    name: 'Arthur',
+    articles: [
+      'https://app.hackernoon.com/drafts/RTLukSZOLY4dA8i54yu3',
+      'https://app.hackernoon.com/drafts/g8yO5OeEVCP76NWxF9PM',
+      'https://app.hackernoon.com/drafts/Uenv0s9AZjeWwqNS3FHF',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm'
+    ]
+  },
+    {
+    key: 8,
+    name: 'Rianke',
+    articles: [
+      'https://app.hackernoon.com/drafts/RTLukSZOLY4dA8i54yu3',
+      'https://app.hackernoon.com/drafts/g8yO5OeEVCP76NWxF9PM',
+      'https://app.hackernoon.com/drafts/Uenv0s9AZjeWwqNS3FHF',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm'
+    ]
+  }
+  ];
+  
+module.exports = { expandable2 };
