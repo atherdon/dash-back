98e94ad3b8bb45eab5723ca928f9c270a66ee238
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-03-16 03:54:45 +0200

    Create expandable3.js

---
+++ data/expandable3.js
@@ -0,0 +1,59 @@
+const expandable3 = [ 
+{
+    key: 1,
+    name: 'Programming',
+    articles: [
+      'https://app.hackernoon.com/drafts/RTLukSZOLY4dA8i54yu3',
+      'https://app.hackernoon.com/drafts/g8yO5OeEVCP76NWxF9PM',
+      'https://app.hackernoon.com/drafts/Uenv0s9AZjeWwqNS3FHF',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm'
+    ]
+  },
+  {
+    key: 2,
+    name: 'Business',
+    articles: [
+      'https://app.hackernoon.com/drafts/RTLukSZOLY4dA8i54yu3',
+      'https://app.hackernoon.com/drafts/g8yO5OeEVCP76NWxF9PM',
+      'https://app.hackernoon.com/drafts/Uenv0s9AZjeWwqNS3FHF',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm'
+    ]
+  },
+  {
+    key: 3,
+    name: 'Repost',
+    articles: [
+      'https://app.hackernoon.com/drafts/RTLukSZOLY4dA8i54yu3',
+      'https://app.hackernoon.com/drafts/g8yO5OeEVCP76NWxF9PM',
+      'https://app.hackernoon.com/drafts/Uenv0s9AZjeWwqNS3FHF',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm'
+    ]
+  },
+  {
+    key: 4,
+    name: 'Blockchain',
+    articles: [
+      'https://app.hackernoon.com/drafts/RTLukSZOLY4dA8i54yu3',
+      'https://app.hackernoon.com/drafts/g8yO5OeEVCP76NWxF9PM',
+      'https://app.hackernoon.com/drafts/Uenv0s9AZjeWwqNS3FHF',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm'
+    ]
+  },
+    {
+    key: 4,
+    name: 'Bad Keywords',
+    articles: [
+      'https://app.hackernoon.com/drafts/RTLukSZOLY4dA8i54yu3',
+      'https://app.hackernoon.com/drafts/g8yO5OeEVCP76NWxF9PM',
+      'https://app.hackernoon.com/drafts/Uenv0s9AZjeWwqNS3FHF',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm'
+    ]
+  }
+];
+
+module.exports = { expandable3 };
