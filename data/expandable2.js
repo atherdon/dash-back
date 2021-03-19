c274928f23a31276b96f583af73ccf22b4dac86a
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-03-19 20:56:33 +0200

    Add files via upload

---
+++ data/expandable2.js
@@ -0,0 +1,100 @@
+const expandable2 = [ 
+  {
+    key: 1,
+    name: 'Amy Tom',
+    address: 'New York No. 1 Lake Park',
+    articles: [
+      'https://app.hackernoon.com/drafts/RTLukSZOLY4dA8i54yu3',
+      'https://app.hackernoon.com/drafts/g8yO5OeEVCP76NWxF9PM',
+      'https://app.hackernoon.com/drafts/Uenv0s9AZjeWwqNS3FHF',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm'
+    ]
+  },
+  {
+    key: 2,
+    name: 'Hackerhodl',
+    address: 'London No. 1 Lake Park',
+    articles: [
+      'https://app.hackernoon.com/drafts/RTLukSZOLY4dA8i54yu3',
+      'https://app.hackernoon.com/drafts/g8yO5OeEVCP76NWxF9PM',
+      'https://app.hackernoon.com/drafts/Uenv0s9AZjeWwqNS3FHF',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm'
+    ]
+  },
+  {
+    key: 3,
+    name: 'Natasha',
+    address: 'Jiangsu No. 1 Lake Park',
+    articles: [
+      'https://app.hackernoon.com/drafts/RTLukSZOLY4dA8i54yu3',
+      'https://app.hackernoon.com/drafts/g8yO5OeEVCP76NWxF9PM',
+      'https://app.hackernoon.com/drafts/Uenv0s9AZjeWwqNS3FHF',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm'
+    ]
+  },
+  {
+    key: 4,
+    name: 'Marina',
+    address: 'Sidney No. 1 Lake Park',
+    articles: [
+      'https://app.hackernoon.com/drafts/RTLukSZOLY4dA8i54yu3',
+      'https://app.hackernoon.com/drafts/g8yO5OeEVCP76NWxF9PM',
+      'https://app.hackernoon.com/drafts/Uenv0s9AZjeWwqNS3FHF',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm'
+    ]
+  },
+    {
+    key: 5,
+    name: 'David',
+    address: 'Sidney No. 1 Lake Park',
+    articles: [
+      'https://app.hackernoon.com/drafts/RTLukSZOLY4dA8i54yu3',
+      'https://app.hackernoon.com/drafts/g8yO5OeEVCP76NWxF9PM',
+      'https://app.hackernoon.com/drafts/Uenv0s9AZjeWwqNS3FHF',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm'
+    ]
+  },
+    {
+    key: 6,
+    name: 'Linh',
+    address: 'Sidney No. 1 Lake Park',
+    articles: [
+      'https://app.hackernoon.com/drafts/RTLukSZOLY4dA8i54yu3',
+      'https://app.hackernoon.com/drafts/g8yO5OeEVCP76NWxF9PM',
+      'https://app.hackernoon.com/drafts/Uenv0s9AZjeWwqNS3FHF',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm'
+    ]
+  },
+    {
+    key: 7,
+    name: 'Arthur',
+    address: 'Sidney No. 1 Lake Park',
+    articles: [
+      'https://app.hackernoon.com/drafts/RTLukSZOLY4dA8i54yu3',
+      'https://app.hackernoon.com/drafts/g8yO5OeEVCP76NWxF9PM',
+      'https://app.hackernoon.com/drafts/Uenv0s9AZjeWwqNS3FHF',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm'
+    ]
+  },
+    {
+    key: 8,
+    name: 'Rianke',
+    address: 'Sidney No. 1 Lake Park',
+    articles: [
+      'https://app.hackernoon.com/drafts/RTLukSZOLY4dA8i54yu3',
+      'https://app.hackernoon.com/drafts/g8yO5OeEVCP76NWxF9PM',
+      'https://app.hackernoon.com/drafts/Uenv0s9AZjeWwqNS3FHF',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm'
+    ]
+  }
+  ];
+  
+module.exports = { expandable2 };
---
+++ data/expandable3.js
@@ -0,0 +1,64 @@
+const expandable3 = [ 
+{
+    key: 1,
+    name: 'Programming',
+    address: 'New York No. 1 Lake Park',
+    articles: [
+      'https://app.hackernoon.com/drafts/RTLukSZOLY4dA8i54yu3',
+      'https://app.hackernoon.com/drafts/g8yO5OeEVCP76NWxF9PM',
+      'https://app.hackernoon.com/drafts/Uenv0s9AZjeWwqNS3FHF',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm'
+    ]
+  },
+  {
+    key: 2,
+    name: 'Business',
+    address: 'London No. 1 Lake Park',
+    articles: [
+      'https://app.hackernoon.com/drafts/RTLukSZOLY4dA8i54yu3',
+      'https://app.hackernoon.com/drafts/g8yO5OeEVCP76NWxF9PM',
+      'https://app.hackernoon.com/drafts/Uenv0s9AZjeWwqNS3FHF',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm'
+    ]
+  },
+  {
+    key: 3,
+    name: 'Repost',
+    address: 'Jiangsu No. 1 Lake Park',
+    articles: [
+      'https://app.hackernoon.com/drafts/RTLukSZOLY4dA8i54yu3',
+      'https://app.hackernoon.com/drafts/g8yO5OeEVCP76NWxF9PM',
+      'https://app.hackernoon.com/drafts/Uenv0s9AZjeWwqNS3FHF',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm'
+    ]
+  },
+  {
+    key: 4,
+    name: 'Blockchain',
+    address: 'Sidney No. 1 Lake Park',
+    articles: [
+      'https://app.hackernoon.com/drafts/RTLukSZOLY4dA8i54yu3',
+      'https://app.hackernoon.com/drafts/g8yO5OeEVCP76NWxF9PM',
+      'https://app.hackernoon.com/drafts/Uenv0s9AZjeWwqNS3FHF',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm'
+    ]
+  },
+    {
+    key: 4,
+    name: 'Bad Keywords',
+    address: 'Sidney No. 1 Lake Park',
+    articles: [
+      'https://app.hackernoon.com/drafts/RTLukSZOLY4dA8i54yu3',
+      'https://app.hackernoon.com/drafts/g8yO5OeEVCP76NWxF9PM',
+      'https://app.hackernoon.com/drafts/Uenv0s9AZjeWwqNS3FHF',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm',
+      'https://app.hackernoon.com/drafts/smj92gpyekO2ANX0h2Qm'
+    ]
+  }
+];
+
+module.exports = { expandable3 };
